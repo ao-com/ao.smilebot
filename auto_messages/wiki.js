a0e9744f075814f99701fb66a2803fec690f0990
shepc <chris.shepherd@ao.com>
2016-06-30 08:56:17 +0100

    Committed changes to wiki.js

--- auto_messages/wiki.js
+++ auto_messages/wiki.js
@@ -1,19 +1,30 @@
 var config = require('../config');
 var logger = require('../logger');
 var utilities = require('../utilities');
+var cheerio = require('cheerio');
 
 /**
  * Initialises the wiki messages module.
  * @param {bot} bot - Bot to send the message.
  * @param {controller} controller - Controller to send the message.
  */
-exports.init = function(bot, controller) {
+exports.init = function (bot, controller) {
     var _this = this;
 
-    controller.hears(['wiki (.*)', 'docs (.*)'], ['direct_message' , 'direct_mention', 'mention'], function(bot, message) {
+    controller.hears(['wiki (.*)', 'docs (.*)'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
         var article = message.match[1];
-        bot.reply(message, 'Placeholder!');
-        
-        var response = utilities.searchWiki(article);
+        var response = utilities.searchWiki(config.WIKI_SEARCH_URL, article);
+        var $ = cheerio.load(response);
+        var links = $(config.WIKI_RESULT_SELECTOR);
+
+        var results = $(links).map(function (i, el) {
+            return $(this).text() + ' ' + config.WIKI_ROOT + encodeURI($(this).attr('href'));
+        }).get().join('\n');
+
+        if (results === '') {
+            bot.reply(message, 'No results!');
+        }
+
+        bot.reply(message, results);
     });
 };
